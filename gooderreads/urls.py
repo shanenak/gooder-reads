5edb55bc5bd71f46c7907152c04dd8d06c258ebe
Shannon Millar <shanenak@gmail.com>
2023-12-01 19:07:14 -0800

    finish search

--- gooderreads/urls.py
+++ gooderreads/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     # ex: /gooderreads/
     path("", views.index, name="index"),
     # ex: /gooderreads/5/
+    path("search/<str:search_term>/", views.search, name="search"),
     path("<int:book_id>/", views.detail, name="detail"),
     # ex: /gooderreads/5/vote/
     path("<int:book_id>/save/", views.save, name="save"),
--- gooderreads/views.py
+++ gooderreads/views.py
@@ -1,40 +1,38 @@
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 
-import json
-from urllib.request import urlopen
+import requests 
+import os
+from dotenv import load_dotenv
+from datetime import date
 
 from .models import Book, Saved
 
 def index(request):
-    latest_book_list = Book.objects.order_by("title")[:5]
+    latest_book_list = list(set(Book.objects.order_by("title")))
     context = {"latest_book_list":latest_book_list}
     return render(request, "gooderreads/index.html", context)
 
-def search(request, title):
-    api = "https://www.googleapis.com/books/v1/volumes?q=isbn:"
-    isbn = input("Enter 10 digit ISBN: ").strip()
+def search(request, search_term):
+    load_dotenv()
+    GOOGLE_KEY = os.environ.get('GOOGLE_API_KEY')
+    parms = {"q": search_term, 'key': GOOGLE_KEY}
+    r = requests.get(url="https://www.googleapis.com/books/v1/volumes", params=parms)
+    rj = r.json()
+    for i in rj["items"][:10]:
+        res_title = i['volumeInfo']['title']
+        res_authors = i['volumeInfo']['authors'] if 'authors' in i['volumeInfo'] else 'No author'
+        if not Book.objects.filter(title__iexact=res_title).exists():
+            print(res_title)
+            new_book = Book.objects.create_book(
+                res_title, " ".join(res_authors))
+            new_book.save()
+    latest_book_list = list(set(Book.objects.filter(
+        title__contains=search_term).order_by("title")))
+    context = {"latest_book_list": latest_book_list}
+    return render(request, "gooderreads/index.html", context)
 
-    # send a request and get a JSON response
-    resp = urlopen(api + isbn)
-    # parse JSON into Python as a dictionary
-    book_data = json.load(resp)
-
-    # create additional variables for easy querying
-    volume_info = book_data["items"][0]["volumeInfo"]
-    author = volume_info["authors"]
-    # practice with conditional expressions!
-    prettify_author = author if len(author) > 1 else author[0]
-
-    # display title, author, page count, publication date
-    # fstrings require Python 3.6 or higher
-    # \n adds a new line for easier reading
-    print(f"\nTitle: {volume_info['title']}")
-    print(f"Author: {prettify_author}")
-    print(f"Page Count: {volume_info['pageCount']}")
-    print(f"Publication Date: {volume_info['publishedDate']}")
-    print("\n***\n")
 
 def detail(request, book_id):
     book = get_object_or_404(Book, pk=book_id)
@@ -42,7 +40,7 @@ def detail(request, book_id):
    
 def save(request, book_id):
     book = get_object_or_404(Book, pk=book_id)
-    new_save = Saved.objects.create_save(book, "11/11/2011", 'Finished', 'Good', 5)
+    new_save = Saved.objects.create_save(book, date.today(), 'Finished', 'Good', 5)
     new_save.save()
     return HttpResponseRedirect(reverse("gooderreads:results", args=(book.id,)))
 
